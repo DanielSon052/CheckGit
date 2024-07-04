bcc33d81441bce17f71c1ab0914d1c6b69c99ea6
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2024-07-05 01:57:22 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -6,3 +6,14 @@ python train.py \
   -n 'test_Varnet' \
   -t '/home/Data/train/' \
   -v '/home/Data/val/'
+  
+# train.sh: 모델 훈련을 실행하기 위한 쉘 스크립트
+
+# python train.py: train.py 스크립트를 실행.
+# -b 1: 배치 사이즈를 1로 설정.
+# -e 5: 에포크 수를 5로 설정.
+# -l 0.001: 학습률을 0.001로 설정.
+# -r 10: 주기적으로 모델을 저장하는 간격을 10으로 설정.
+# -n 'test_Varnet': 네트워크 이름을 'test_Varnet'으로 설정.
+# -t '/home/Data/train/': 학습 데이터의 경로를 '/home/Data/train/'으로 설정.
+# -v '/home/Data/val/': 검증 데이터의 경로를 '/home/Data/val/'으로 설정.
